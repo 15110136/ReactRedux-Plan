2ce0d698ee14ac102c155979f10dc6e561756dc0
Dam Nhat Thong <nhatthong34@gmail.com>
2018-11-26 17:58:42 +0700

    lesson-12

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from './components/layout/NavBar'
+import DashBoard from './components/dashboard/DashBoard';
+import ProjectDetail from './components/projects/ProjectDetail';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <NavBar />
+          <Switch>
+            <Route exact path='/' component={DashBoard} />
+            <Route path='/project/:id' component={ProjectDetail} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import SignedInLink from './SignedInLink';
+import SignedOutLinks from './SignedOutLink';
 const NavBar=()=>{
   return(
     <nav className="nav-wrapper grey darken-3">
@@ -9,6 +10,7 @@ const NavBar=()=>{
           Kai'Sa Plan
         </Link>
         <SignedInLink/>
+        <SignedOutLinks/>
       </div>
     </nav>
   );
